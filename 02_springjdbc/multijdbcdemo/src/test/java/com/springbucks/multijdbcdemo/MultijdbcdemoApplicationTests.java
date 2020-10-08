09f5a9b658b0797cde43159605fda8e5320c62aa
leesper <pascal7718@gmail.com>
2020-10-08 13:01:40 +0800

    feat: Add demo for multi data source

---
+++ 02_springjdbc/multijdbcdemo/src/test/java/com/springbucks/multijdbcdemo/MultijdbcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbucks.multijdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultijdbcdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
