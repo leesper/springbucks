6fc9fe340092dd51424fd0eed4dc5fcc72ef6979
leesper <pascal7718@gmail.com>
2020-10-09 22:59:24 +0800

    feat: Add demo for jdbc operations

---
+++ 02_springjdbc/simplejdbcdemo/src/test/java/com/springbucks/simplejdbcdemo/SimplejdbcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbucks.simplejdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplejdbcdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
